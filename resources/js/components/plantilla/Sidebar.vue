d703b16aaf174667a68905a4b76e6c2f905b9b70
= <=>
2020-11-08 11:21:50 -0500

    problema de logo

--- resources/js/components/plantilla/Sidebar.vue
+++ resources/js/components/plantilla/Sidebar.vue
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4 text-sm">
         <!-- Brand Logo -->
         <a href="../../index3.html" class="brand-link">
-        <img :src="ruta + '/img/adminLTELogo.png'"
+        <img :src="ruta + '/img/AdminLTELogo.png'"
             alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3"
             style="opacity: .8">
