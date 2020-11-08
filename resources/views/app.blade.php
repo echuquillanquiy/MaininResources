4e34495bddc6fd81a095e8c9ba480d3deb312dc1
= <=>
2020-11-07 23:50:54 -0500

    Inicial

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <body class="hold-transition sidebar-mini">
 <div class="wrapper" id="app">
   <App ruta="{{route('basepath')}}"></App>
-  
+  <h1>HOLA COMO ESTAS</h1>
 </div>
   @include('sections.script')
 
